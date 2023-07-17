bce166faaa9de5ccc37978a5af47e234851d296c
Andrew Desmarais <andydes@amazon.com>
2023-07-17 17:38:16 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code> - OH YEAH! { num }
+            <code className={styles.code}>pages/index.tsx</code> - Update 2023-07-17 { num }
           </p>
           <div>
             <a
