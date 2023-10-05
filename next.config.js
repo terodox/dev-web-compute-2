985ecb4a4c51b96f3674935e7283e8a718708499
terodox <andydes@amazon.com>
2023-10-05 09:08:07 -0400

    Base path it up

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/andy-base-path',
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'source.unsplash.com'],
+    domains: ['images.unsplash.com', 'source.unsplash.com', 'skylark-doc.s3.ca-central-1.amazonaws.com'],
   },
 };
 
--- src/pages/pillars.tsx
+++ src/pages/pillars.tsx
@@ -12,14 +12,20 @@ export default function Home() {
           name="description"
           content="An optimized picture of the pillars of creation"
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+        />
       </Head>
       <main className={styles.main}>
         <figure>
           <figcaption>Using path</figcaption>
           <Image
-            src="/pillars-of-creation.png"
+            src="/andy-base-path/pillars-of-creation.png"
             alt="The pillars of creation"
             height={300}
             width={900}
@@ -38,9 +44,17 @@ export default function Home() {
           <figcaption>Unsplash</figcaption>
           <Image
             src="https://images.unsplash.com/photo-1520283451192-c3b05d7db25b"
-            alt="Pillars"
+            alt="Unsplash"
             height={300}
             width={900}
+          ></Image>
+        </figure>
+        <figure>
+          <figcaption>BIG AVIF</figcaption>
+          <Image
+            src="https://skylark-doc.s3.ca-central-1.amazonaws.com/profile/1694153026653577.jpg"
+            alt="Big Avif"
+            width={1920}
           ></Image>
         </figure>
       </main>
