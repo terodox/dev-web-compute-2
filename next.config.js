9a461f86c8a38c3abcf06d8705635d4951465047
terodox <andydes@amazon.com>
2023-10-05 09:58:15 -0400

    no more base path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/andy-base-path',
+  // basePath: '/andy-base-path',
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'source.unsplash.com', 'skylark-doc.s3.ca-central-1.amazonaws.com'],
