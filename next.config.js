47e2c2461bc7b23f19179638895c692581e39b78
terodox <andydes@amazon.com>
2023-10-06 14:41:07 -0400

    base path it up

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: '/andy-base-path',
+  basePath: '/andy-base-path',
+  // assetPrefix: 'http://andy.test/asset-prefix',
+  // assetPrefix: '/some-asset-prefix',
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'source.unsplash.com', 'skylark-doc.s3.ca-central-1.amazonaws.com'],
