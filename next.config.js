3e2ab92aa044cbe8c9c618ffd7052ccb250bdc70
Andrew Desmarais <andydes@amazon.com>
2023-10-17 16:33:21 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
       'images.unsplash.com', 
       'source.unsplash.com', 
       'skylark-doc.s3.ca-central-1.amazonaws.com',
-      'main.d3fkcxokc1esm5.amplifyapp.com/'
+      'main.d3fkcxokc1esm5.amplifyapp.com'
     ],
   },
 };
